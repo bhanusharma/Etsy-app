683da2685b762a3079840094fc1ca4f1dbbb251f
Bhanu Sharma <bhanu@Bhanus-MacBook-Air.local>
2012-01-21 17:00:11 -0800

    added first project files

---
+++ EtsyApp/EtsyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EtsyApp
+//
+//  Created by Bhanu Sharma on 1/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ EtsyApp/EtsyAppTests/EtsyAppTests.m
@@ -0,0 +1,32 @@
+//
+//  EtsyAppTests.m
+//  EtsyAppTests
+//
+//  Created by Bhanu Sharma on 1/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "EtsyAppTests.h"
+
+@implementation EtsyAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EtsyAppTests");
+}
+
+@end
